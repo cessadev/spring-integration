207e2a1d1f29cd4aa991e92b91244163038f6659
cessadev <cesarurb.dev@gmail.com>
2024-10-17 15:09:24 -0500

    First commit

---
+++ src/main/java/com/cessadev/demo_spring_integration/DemoSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.cessadev.demo_spring_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cessadev/demo_spring_integration/DemoSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cessadev.demo_spring_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
